54768a1e54ed1477bd8b3cd34116bac05acadf26
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-01 18:38:43 -0300

    feat: implementation of token tests

--- backend/src/tests/services/login.spec.js
+++ backend/src/tests/services/login.spec.js
@@ -1,6 +1,7 @@
 const loginService = require('../../api/services/loginService');;
 const { userMock } = require('../mocks/users');
 const { User } = require('../../database/models');
+const jwt = require('../../utils/jwt');
 
 describe('Testes da camada "services" de Login: ', () => {
   beforeEach(() => {
@@ -9,13 +10,15 @@ describe('Testes da camada "services" de Login: ', () => {
   });
 
   it('Existe a service com o nome de signIn', () => {
-    expect(typeof loginService.signIn).toBe('function')
+    expect(typeof loginService.signIn).toBe('function');
   });
 
   it('Retorna um token válido', async () => {
     const response = await loginService.signIn({ email: userMock.email, password: userMock.password });
     expect(response).toBeDefined();
 
-    
+    const decodedToken = jwt.decodeToken(response);
+
+    expect(decodedToken).toEqual({ id: userMock.email, email: userMock.email });
   });
 });
