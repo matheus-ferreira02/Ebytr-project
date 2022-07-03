d758d7114989b7835449d93faa04c979fc791594
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-03 02:27:40 -0300

    feat: generic error middleware added to API

--- backend/src/api/routes/login/index.js
+++ backend/src/api/routes/login/index.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 const loginController = require('../../controllers/login');
 const rescue = require('express-rescue');
+const validateLogin = require('../../middlewares/validateLogin');
 
-routes.post('/', rescue(loginController.signIn));
+routes.post('/', rescue(validateLogin), rescue(loginController.signIn));
 
 module.exports = routes;
