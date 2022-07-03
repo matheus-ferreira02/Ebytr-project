094f8ed1db5aa1141f8df7a26d9e0ca6fa5c4fa0
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-03 16:45:36 -0300

    login inputs with size and format validations

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import { useState, useRef } from 'react';
 
 function Login() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [disabledSubmit, setDisabledSubmit] = useState(true);
+
+  const signIn = (event) => {
+    event.preventDefault();
+    console.log('ola');
+  };
+
+  const verifyFields = () => {
+    const { value: email } = emailRef.current;
+    const { value: password } = passwordRef.current;
+
+    const regexEmail = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+    const isValidEmail = regexEmail.test(email);
+    const isValidPassword = password.length >= 6;
+
+    const isValidFields = [isValidEmail, isValidPassword];
+
+    setDisabledSubmit(!isValidFields.every((validField) => validField));
+  };
+
   return (
-    <div>Login</div>
+    <section>
+      <form onSubmit={ signIn }>
+        <input
+          onChange={ verifyFields }
+          type="text" placeholder='Email'
+          ref={ emailRef }
+        />
+
+        <input
+          onChange={ verifyFields }
+          type="text"
+          placeholder='Senha'
+          ref={ passwordRef }
+        />
+
+        <button type="submit" disabled={ disabledSubmit }>Entrar</button>
+      </form>
+    </section>
   );
 }
 
