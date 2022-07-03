63ce3156340970435f3cb361555300da208025a6
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-03 02:03:53 -0300

    fix: Sequelize syntax in service layer

--- backend/src/api/services/login.js
+++ backend/src/api/services/login.js
@@ -3,7 +3,7 @@ const { User } = require('../../database/models');
 const createObjError = require('../../utils/createObjError');
 
 const signIn = async ({ email, password }) => {
-  const user = await User.findOne({ email, password });
+  const user = await User.findOne({ where: { email, password } });
   if (!user) throw createObjError(404, 'User not found');
   const token = jwt.generateToken(user);
 
