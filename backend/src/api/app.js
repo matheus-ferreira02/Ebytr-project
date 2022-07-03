fe34fb695750f9fb6965ae94830c65a65ed7a3b4
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-03 02:02:35 -0300

    feat: login route added to API

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -4,6 +4,10 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  return res.send('API online');
+});
+
 app.use(routes);
 
 module.exports = app;
