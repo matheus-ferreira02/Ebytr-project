9ea7291e7a7746cf1b3376141eeacee3dd0acb09
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-03 00:55:30 -0300

    login service returns an error, created in TDD

--- backend/src/api/services/login.js
+++ backend/src/api/services/login.js
@@ -1,8 +1,11 @@
 const jwt = require('../../utils/jwt');
 const { User } = require('../../database/models');
+const createObjError = require('../../utils/createObjError');
 
 const signIn = async ({ email, password }) => {
   const user = await User.findOne({ email, password });
+
+  if (!user) throw createObjError(404, 'User not found');
 
   const token = jwt.generateToken(user);
 
--- backend/src/tests/services/login.test.js
+++ backend/src/tests/services/login.test.js
@@ -1,24 +1,49 @@
-const loginService = require('../../api/services/loginService');
+const loginService = require('../../api/services/login');
 const { userMock } = require('../mocks/users');
 const { User } = require('../../database/models');
 const jwt = require('../../utils/jwt');
+const createObjError = require('../../utils/createObjError');
 
 describe('Testes da camada "services" de Login: ', () => {
-  beforeEach(() => {
-    User.findOne = jest.fn().mockResolvedValue(userMock);
-    jest.mock('../../utils/generateToken');
+  describe('Em casos de sucesso, testa se', () => {
+    beforeEach(() => {
+      User.findOne = jest.fn().mockResolvedValue(userMock);
+    });
+
+    afterEach(() => {
+      User.findOne.mockReset();
+    });
+
+    it('existe a service com o nome de signIn', () => {
+      expect(typeof loginService.signIn).toBe('function');
+    });
+
+    it('retorna um token válido', async () => {
+      const response = await loginService.signIn({ email: userMock.email, password: userMock.password });
+      expect(response).toBeDefined();
+  
+      const decodedToken = jwt.decodeToken(response);
+  
+      expect(decodedToken).toEqual({ id: userMock.id, email: userMock.email });
+    });
   });
 
-  it('Existe a service com o nome de signIn', () => {
-    expect(typeof loginService.signIn).toBe('function');
-  });
+  describe('Em casos de erro, testa se', () => {
+    beforeEach(() => {
+      User.findOne = jest.fn().mockResolvedValue(null);
+    });
 
-  it('Retorna um token válido', async () => {
-    const response = await loginService.signIn({ email: userMock.email, password: userMock.password });
-    expect(response).toBeDefined();
+    afterEach(() => {
+      User.findOne.mockReset();
+    });
 
-    const decodedToken = jwt.decodeToken(response);
-
-    expect(decodedToken).toEqual({ id: userMock.id, email: userMock.email });
+    it('retorna um erro válido com status "404" e messagem "User not found"', async () => {
+      try {
+        await loginService.signIn({ email: userMock.email, password: userMock.password })
+      } catch (error) {
+        expect(error.status).toBe(404);
+        expect(error.message).toBe('User not found');
+      }
+    });
   });
 });
