002a359b4a951d74567628aef0352fc3fe1291d9
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-04 19:40:21 -0300

    refact: task status in portuguese

--- backend/src/database/seeders/20220626185549-task.js
+++ backend/src/database/seeders/20220626185549-task.js
@@ -6,25 +6,25 @@ module.exports = {
       {
         title: 'Organizar papelada',
         content: 'Organizar a papelada da reunião da diretoria',
-        status: 'pending',
+        status: 'pendente',
         user_id: 1
       },
       {
         title: 'Assinar contratações',
         content: 'Assinar carteiras de trabalhos dos novos funcionários',
-        status: 'completed',
+        status: 'completa',
         user_id: 1
       },
       {
         title: 'Preparar slide',
         content: 'Preparar apresentação para a reunião da diretoria',
-        status: 'progress',
+        status: 'em andamento',
         user_id: 2
       },
       {
         title: 'Apresentar empresa',
         content: 'Apresentar empresa para os novos funcionários',
-        status: 'pending',
+        status: 'pendente',
         user_id: 2
       },
     ], {
