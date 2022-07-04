89b94782767d95bb54e29e6e651c47d29a4eecb8
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-04 18:52:05 -0300

    refact: service that takes tasks now returns ordered tasks

--- backend/src/api/services/task.js
+++ backend/src/api/services/task.js
@@ -1,7 +1,13 @@
 const { Task } = require('../../database/models');
 
 const getTasksByUserId = async (userId) => {
-  const response = await Task.findAll({ where: { userId } });
+  const response = await Task.findAll({ where: { userId },
+    order: [
+      ['title', 'ASC'],
+      ['createdAt', 'ASC'],
+      ['status', 'ASC'],
+    ]
+  });
 
   return response;
 };
